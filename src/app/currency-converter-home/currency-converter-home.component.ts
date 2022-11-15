6db0debd60893fef85be363782316a9422b23028
geeks-rishabh <rishabh.awasthi@geeksinvention.com>
2022-11-15 17:25:54 +0530

    Static responses

--- src/app/currency-converter-home/currency-converter-home.component.ts
+++ src/app/currency-converter-home/currency-converter-home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { threadId } from 'worker_threads';
 import { ApiService } from '../shared/services/api.service';
 
 @Component({
@@ -13,6 +12,7 @@ export class CurrencyConverterHomeComponent implements OnInit {
   currentValue: string = "";
   conversionRate = "";
   currencies: any[] = [{ name: 'United States Dollar', symbol: 'USD' }, { name: 'Great Britain Pound', symbol: 'GBP' }, { name: 'Euro', symbol: 'EUR' }];
+  latestRates: any[] = [];
   constructor(private fb: FormBuilder, private apiService: ApiService) { }
 
   ngOnInit(): void {
@@ -22,18 +22,33 @@ export class CurrencyConverterHomeComponent implements OnInit {
       to: new FormControl('USD', [Validators.required]),
     });
 
-     this.fetchCurrencyList(); 
+    // this.fetchCurrencyList();
   }
   convertCurrency() {
     console.log(this.currencyForm.value);
     if (!this.currencyForm.valid) {
       return;
     }
-
-    this.apiService.getExchangeRates(this.currencyForm.value).subscribe((res: any) => {
-      this.currentValue = res.result;
-      this.conversionRate = res.info.rate;
-    });
+    const conversionResponse = {
+      "date": "2018-02-22",
+      "historical": "",
+      "info": {
+        "rate": 148.972231,
+        "timestamp": 1519328414
+      },
+      "query": {
+        "amount": 25,
+        "from": "GBP",
+        "to": "JPY"
+      },
+      "result": 3724.305775,
+      "success": true
+    }
+    this.currentValue = "" + conversionResponse.result;
+    this.conversionRate = "" + conversionResponse.info.rate;
+    this.fetchLatestConversionRates(); 
+       // this.apiService.getExchangeRates(this.currencyForm.value).subscribe((res: any) => {
+    // });
   }
 
   fetchCurrencyList() {
@@ -41,16 +56,48 @@ export class CurrencyConverterHomeComponent implements OnInit {
     let names: string[] = [];
     this.apiService.getCurrencyList().subscribe((res: any) => {
       // this.currencies = res.symbols;
-
+      this.fetchLatestConversionRates();
       names = Object.values(res.symbols);
       symbols = Object.keys(res.symbols);
-this.currencies = [];
-console.log(names, symbols);
+      this.currencies = [];
+
       for (let i = 0; i < names.length; i++) {
-        console.log("inside", { name: names[i], symbol: symbols[i] });
         this.currencies.push({ name: names[i], symbol: symbols[i] });
-     console.log(this.currencies); }
+      }
     });
+  }
+  fetchLatestConversionRates() {
+    const res = {
+      "base": "USD",
+      "date": "2022-04-14",
+      "rates": {
+        "EUR": 0.813399,
+        "GBP": 0.72007,
+        "JPY": 107.346001
+      },
+      "success": true,
+      "timestamp": 1519296206
+    }
+    let symbols: string[] = [];
+    let rates: number[] = [];
+    symbols = Object.keys(res.rates);
+    rates = Object.values(res.rates);
+
+    // this.currencies = [];
+
+    for (let i = 0; i < symbols.length; i++) {
+
+      if ((this.currencyForm.value.to ===symbols[i]) || (this.currencyForm.value.from ===symbols[i])) {
+
+      } else { this.latestRates.push({ rate: rates[i], symbol: symbols[i] }); }
+    }
+
+    // this.apiService.getLatestConversionRate().subscribe((res: any) => {
+
+
+    //   console.log(res);
+
+    // });
 
   }
 
