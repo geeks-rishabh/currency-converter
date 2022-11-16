36cdffc8a047bbf9a2238c73fcc1aee89b3cf74c
geeks-rishabh <rishabh.awasthi@geeksinvention.com>
2022-11-16 11:20:48 +0530

    Ng2 Charts dependency added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CurrencyConverterHomeComponent } from './currency-converter-home/currency-converter-home.component';
+import { CurrencyDetailComponent } from './currency-detail/currency-detail.component';
 
-const routes: Routes = [{path:'home',component:CurrencyConverterHomeComponent}];
+const routes: Routes = [{ path: 'home', component: CurrencyConverterHomeComponent }, { path: 'details', component: CurrencyDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
